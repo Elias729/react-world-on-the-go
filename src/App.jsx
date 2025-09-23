a1c4aa3a2ff871a83c12ae3f41f2e45405111c49
Elias729 <eliashossainrafe72@gmail.com>
2025-09-24 02:23:08 +0600

    steart the project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Countries from "./componets/countries/countries";
+
+function App() {
+  return (
+    <>
+      <Countries></Countries>
+    </>
+  );
+}
+
+export default App;
