ea654afe02b924a3ef1d6438eb3f288474e54d4e
Elias729 <eliashossainrafe72@gmail.com>
2025-09-24 12:36:27 +0600

    added country flags details and complete design

--- src/componets/Country/Country.jsx
+++ src/componets/Country/Country.jsx
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Country.css";
 
 const Country = ({ country }) => {
   console.log(country);
 
+  const [visited, setVisited] = useState(false);
+
+  const handelVisited = () => {
+    setVisited(visited ? false : true);
+  };
+
   return (
     <>
-      <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
-      <h1>Name: {country.name.common}</h1>
-      <p>Population: {country.population.population}</p>
-      <p>Area: {country.area.area}</p>
+      <div
+        className={`country bg-white border border-gray-100 p-4 m-4 rounded-2xl shadow-lg hover:scale-105 duration-300 flex flex-col justify-center items-start ${
+          visited && "visited"
+        }`}
+      >
+        <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
+        <h1 className="text-xl font-bold mt-2">Name: {country.name.common}</h1>
+        <p className="text-lg font-semibold mt-2">
+          Population: {country.population.population}
+        </p>
+        <p className="text-lg font-semibold mt-2">
+          Area: {country.area.area}
+          {country.area.area > 300000 ? "Large Country" : "Small Country"}{" "}
+        </p>
+        <button
+          className="bg-blue-500 text-white px-4 py-2 rounded cursor-pointer mt-4"
+          onClick={handelVisited}
+        >
+          {visited ? "Visited" : "Not Visited"}
+        </button>
+      </div>
     </>
   );
 };
--- src/componets/countries/countries.jsx
+++ src/componets/countries/countries.jsx
@@ -7,11 +7,15 @@ const Countries = ({ countryPromise }) => {
 
   return (
     <>
-      <h2>Countries: {countries.length} </h2>
+      <h2 className="text-2xl font-bold mb-4 text-center mt-5 bg-[#ec0b43] text-white p-3 rounded-lg">
+        Countries: {countries.length}{" "}
+      </h2>
 
-      {countries.map((country) => (
-        <Country key={country.cca3.cca3} country={country}></Country>
-      ))}
+      <div className="countries-container grid-cols-1 grid md:grid-cols-3 gap-4 justify-items-center">
+        {countries.map((country) => (
+          <Country key={country.cca3.cca3} country={country}></Country>
+        ))}
+      </div>
     </>
   );
 };
